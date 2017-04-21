15d491596d2a8c881d25f3fb7457d8d12b9299d1
Adrian Herrera <adrian.herrera02@gmail.com>
2017-04-21 10:43:21 +0200

    string_checkers: added triple-quote check

--- string_checkers.py
+++ string_checkers.py
@@ -9,6 +9,13 @@ from pylint.checkers import BaseTokenChecker
 from pylint.interfaces import ITokenChecker
 
 
+SINGLE_QUOTES = ('\'', '"')
+TRIPLE_QUOTES = ('\'\'\'', '"""')
+
+DEFAULT_SINGLE_QUOTE = '\''
+DEFAULT_TRIPLE_QUOTE = '"""'
+
+
 class LiteralQuoteChecker(BaseTokenChecker):
     """
     Check that string literals use a consistent quote character, be it a single
@@ -24,12 +31,22 @@ class LiteralQuoteChecker(BaseTokenChecker):
                       'Used when the string literal quote character does not '
                       'match the one specified in the '
                       '`expected-string-literal-quote` option.'),
+            'C8002': ('Use `%s` triple-quotes, not `%s`.',
+                      'incorrect-triple-quotes',
+                      'Used when the triple-quotes character does not match '
+                      'the one specified in the `expected-triple-quote` '
+                      'option.'),
            }
     options = (('expected-string-literal-quote',
-                {'type': 'choice', 'metavar': '<\' or ">', 'default': '\'',
-                 'choices': ['\'', '"'],
+                {'type': 'choice', 'metavar': '<\' or ">',
+                 'default': DEFAULT_SINGLE_QUOTE, 'choices': SINGLE_QUOTES,
                  'help': 'The default string literal quote character. Must be '
                          'either \' or "'}),
+               ('expected-string-triple-quote',
+                {'type': 'choice', 'metavar': '<\'\'\' or """>',
+                 'default': DEFAULT_TRIPLE_QUOTE, 'choices': TRIPLE_QUOTES,
+                 'help': 'The default string triple quote. Must be either '
+                         '\'\'\' or """'}),
               )
 
     def process_tokens(self, tokens):
@@ -38,27 +55,29 @@ class LiteralQuoteChecker(BaseTokenChecker):
                 self._process_string_token(token, start_row)
 
     def _process_string_token(self, token, start_row):
+        expected_quote_char = self.config.expected_string_literal_quote
+        expected_triple_quotes = self.config.expected_string_triple_quote
+
         # Adapted from pylint/checkers/strings.py
         for i, c in enumerate(token):
-            if c in ('\'', '"'):
-                quote_char = c
+            if c in SINGLE_QUOTES:
                 break
 
         # pylint: disable=undefined-loop-variable
         # We ignore prefix markers like u, b, r
         after_prefix = token[i:]
 
-        # Ignore triple quoted strings
-        if len(after_prefix) >= 3 and after_prefix[:3] in ('\'\'\'', '"""'):
-            return
-
-        quote_char = after_prefix[0]
-        expected_quote_char = self.config.expected_string_literal_quote
-
-        if quote_char != expected_quote_char:
+        # Check triple-quote strings
+        if len(after_prefix) >= 3 and after_prefix[:3] in TRIPLE_QUOTES:
+            if after_prefix[:3] != expected_triple_quotes:
+                self.add_message('incorrect-triple-quotes',
+                                 line=start_row,
+                                 args=(expected_triple_quotes, after_prefix[:3]))
+        # Check single quote strings
+        elif after_prefix[0] != expected_quote_char:
             self.add_message('incorrect-string-literal-quote',
                              line=start_row,
-                             args=(expected_quote_char, quote_char))
+                             args=(expected_quote_char, after_prefix[0]))
 
 
 def register(linter):
